8c94dd8622d7da5ff442270ebdedcf02e3cdce9e
Max Delaney <maxdelaney16@gmail.com>
2020-12-27 17:16:30 +0000

    Added hamburger menu

--- app/src/main/java/com/dcs/dcsbeginneraid/Menu.java
+++ app/src/main/java/com/dcs/dcsbeginneraid/Menu.java
@@ -1,17 +1,39 @@
 package com.dcs.dcsbeginneraid;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import android.os.Bundle;
 import android.content.Intent;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class Menu extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class Menu extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    private DrawerLayout drawer;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
         ImageButton plane_button = (ImageButton)findViewById(R.id.plane_button);
         plane_button.setOnClickListener(new View.OnClickListener() {
@@ -54,5 +76,35 @@ public class Menu extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    /*
+       onNavigationItemSelected used to give functionality to items inside the hamburger menu.
+       Add case for each item in the list.
+     */
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.signout:
+                Toast.makeText(this, "Logged out!", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(Menu.this, MainActivity.class));
+                break;
+            case R.id.contact_me:
+                startActivity(new Intent(Menu.this, MainActivity.class));
+                break;
+            case R.id.credits:
+                startActivity(new Intent(Menu.this, MainActivity.class));
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed(){
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
     }
 }
